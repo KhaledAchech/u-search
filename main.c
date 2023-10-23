07003816963fd99cf47745a1d78de5e34a931eca
KhaledAchech <achechkhaled@gmail.com>
2023-10-23 20:23:08 +0200

    fixing formatting

--- main.c
+++ main.c
@@ -9,18 +9,17 @@ void init() {
 
 void banner() {
 	printf("\n");
-    printf("     /\\__\\         /\\  \\         /\\  \\         /\\  \\         /\\  \\    \n");
-    printf("    /:/  /        /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\   \n");
-    printf("   /:/  /        /:/\\ \\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\  \n");
-    printf("  /:/  /  ___   _\\:\\~\\ \\  \\   /::\\~\\:\\  \\   /::\\~\\:\\  \\   /::\\~\\:\\  \\ \n");
-    printf(" /:/__/  /\\__\\ /\\ \\:\\ \\ \\__\\ /:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\\n");
-    printf(" \\:\\  \\ /:/  / \\:\\ \\:\\ \\/__/ \\:\\~\\:\\ \\/__/ \\/__\\:\\/:/  / \\/_|::\\:/  / \n");
-    printf("  \\:\\  /:/  /   \\:\\ \\:\\__\\    \\:\\ \\:\\__\\        \\::/  /     |:|::/  /  \n");
-    printf("   \\:\\/:/  /     \\:\\/:/  /     \\:\\ \\/__/        /:/  /      |:|\\/__/   \n");
-    printf("    \\::/  /       \\::/  /       \\:\\__\\         /:/  /       |:|  |     \n");
-    printf("     \\/__/         \\/__/         \\/__/         \\/__/         \\|__|     \n");
-    printf("\n");
-
+	printf("     /\\__\\         /\\  \\         /\\  \\         /\\  \\         /\\  \\    \n");
+	printf("    /:/  /        /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\   \n");
+	printf("   /:/  /        /:/\\ \\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\  \n");
+	printf("  /:/  /  ___   _\\:\\~\\ \\  \\   /::\\~\\:\\  \\   /::\\~\\:\\  \\   /::\\~\\:\\  \\ \n");
+	printf(" /:/__/  /\\__\\ /\\ \\:\\ \\ \\__\\ /:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\\n");
+	printf(" \\:\\  \\ /:/  / \\:\\ \\:\\ \\/__/ \\:\\~\\:\\ \\/__/ \\/__\\:\\/:/  / \\/_|::\\:/  / \n");
+	printf("  \\:\\  /:/  /   \\:\\ \\:\\__\\    \\:\\ \\:\\__\\        \\::/  /     |:|::/  /  \n");
+	printf("   \\:\\/:/  /     \\:\\/:/  /     \\:\\ \\/__/        /:/  /      |:|\\/__/   \n");
+	printf("    \\::/  /       \\::/  /       \\:\\__\\         /:/  /       |:|  |     \n");
+	printf("     \\/__/         \\/__/         \\/__/         \\/__/         \\|__|     \n");
+	printf("\n");
 }
 
 void positionText(int col) {
@@ -29,30 +28,30 @@ void positionText(int col) {
 
 void printDate() {
 	time_t t = time(NULL);
-  	struct tm tm = *localtime(&t);
-  	positionText(5);
-  	printf("Date: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	struct tm tm = *localtime(&t);
+	positionText(5);
+	printf("Date: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
 
 void search() {
 	char *fName;
 	positionText(5);
-   	printf("What file are you looking for today? \n");
-   	positionText(5);
-   	scanf("%s", fName);
+	printf("What file are you looking for today? \n");
+	positionText(5);
+	scanf("%s", fName);
    	
-   	// On macOS and Linux // TO DO: to be updated spoonnn!
-    #ifdef __unix__
-    const char *command = "find / -name your_file_name.txt 2>/dev/null";
-    #endif
-   	
+	// On macOS and Linux // TO DO: to be updated spoonnn!
+	#ifdef __unix__
+		const char *command = "find / -name your_file_name.txt 2>/dev/null";
+	#endif
 }
 
 int main() {
+	
 	init();
-   	banner();
-   	printDate();
-   	search();
+	banner();
+	printDate();
+	search();
    	
    	return 0;
 }
