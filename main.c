9cdffb0df2d47ad365bfdae1626a46284b77c38a
KhaledAchech <achechkhaled@gmail.com>
2023-10-23 20:19:34 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+void init() {
+	system("cls");
+	system("color 0A");
+}
+
+void banner() {
+	printf("\n");
+    printf("     /\\__\\         /\\  \\         /\\  \\         /\\  \\         /\\  \\    \n");
+    printf("    /:/  /        /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\   \n");
+    printf("   /:/  /        /:/\\ \\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\  \n");
+    printf("  /:/  /  ___   _\\:\\~\\ \\  \\   /::\\~\\:\\  \\   /::\\~\\:\\  \\   /::\\~\\:\\  \\ \n");
+    printf(" /:/__/  /\\__\\ /\\ \\:\\ \\ \\__\\ /:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\\n");
+    printf(" \\:\\  \\ /:/  / \\:\\ \\:\\ \\/__/ \\:\\~\\:\\ \\/__/ \\/__\\:\\/:/  / \\/_|::\\:/  / \n");
+    printf("  \\:\\  /:/  /   \\:\\ \\:\\__\\    \\:\\ \\:\\__\\        \\::/  /     |:|::/  /  \n");
+    printf("   \\:\\/:/  /     \\:\\/:/  /     \\:\\ \\/__/        /:/  /      |:|\\/__/   \n");
+    printf("    \\::/  /       \\::/  /       \\:\\__\\         /:/  /       |:|  |     \n");
+    printf("     \\/__/         \\/__/         \\/__/         \\/__/         \\|__|     \n");
+    printf("\n");
+
+}
+
+void positionText(int col) {
+	printf("\033[%dC", col);
+}
+
+void printDate() {
+	time_t t = time(NULL);
+  	struct tm tm = *localtime(&t);
+  	positionText(5);
+  	printf("Date: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+}
+
+void search() {
+	char *fName;
+	positionText(5);
+   	printf("What file are you looking for today? \n");
+   	positionText(5);
+   	scanf("%s", fName);
+   	
+   	// On macOS and Linux // TO DO: to be updated spoonnn!
+    #ifdef __unix__
+    const char *command = "find / -name your_file_name.txt 2>/dev/null";
+    #endif
+   	
+}
+
+int main() {
+	init();
+   	banner();
+   	printDate();
+   	search();
+   	
+   	return 0;
+}
